06674fa8224a1eba21c15bb1e0bfc3f9b6375b17
Rohan Fating <gaurirohan@Gauris-MacBook-Pro.local>
2018-04-05 09:56:28 +0100

    (feat): Implemented cake detail screen and component to render cake reviews and comment (feature remaining): unit testing (file modified): cake-details.component, cake-reviews.component, style.scss

--- cake-app/src/app/cake/components/cake-details/cake-details.component.ts
+++ cake-app/src/app/cake/components/cake-details/cake-details.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CakeService } from '../../services/cake.service';
+import { CakeInfo } from '../../interfaces/cake.interface';
+import { AppConstant } from '../../constants/constants';
+/**
+ * CakeDetailsComponent to show cake details and review section
+ */
 @Component({
   selector: 'app-cake-details',
-  templateUrl: './cake-details.component.html',
-  styleUrls: ['./cake-details.component.scss']
+  templateUrl: './cake-details.component.html'
 })
-export class CakeDetailsComponent implements OnInit {
+export class CakeDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // Holds selected cake information
+  public cakeInfo: CakeInfo;
 
-  ngOnInit() {
+  // To show error message if data unavailable
+  public errorMessage: string;
+
+  private activatedRoute: ActivatedRoute;
+  private cakeService: CakeService;
+
+  /**
+   * @constructor
+   * @param activatedRoute - to read query param
+   * @param router - to navigate to other pages
+   * @param cakeService - service to get detail info
+   */
+  constructor(activatedRoute: ActivatedRoute, cakeService: CakeService) {
+    this.activatedRoute = activatedRoute;
+    this.cakeService = cakeService;
+    this.cakeInfo = {
+      id: '',
+      comment: '',
+      yumFactor: 0,
+      imageUrl: '',
+      name: ''
+    };
   }
 
+  /**
+   * Lifecycle Hook ngOnInit
+   */
+  public ngOnInit() {
+    this.errorMessage = '';
+    const id: string = this.activatedRoute.snapshot.params.id;
+    this.cakeService.getCakeDetailsById(id).subscribe(
+      (data) => {
+        this.cakeInfo = data;
+      },
+      (err) => {
+        this.errorMessage = AppConstant.ERROR_MESSAGE;
+      }
+    );
+  }
+
+  /**
+   * To get data updated after submitting comment from cake reviews component
+   * @param data - CakeInfo data coming after submitting reviews
+   */
+  public updateCakeInfo(data: CakeInfo): void {
+    this.cakeInfo = data;
+  }
+
+  /**
+   * Lifecycle Hook ngOnDestroy to clean up component
+   */
+  public ngOnDestroy(): void {
+    this.cakeInfo = null;
+  }
 }
